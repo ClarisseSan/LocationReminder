6f73f9fe3b01fb09a89b727505b49389e81441ac
Clarisse Sanchez <sanchezclarisse@gmail.com>
2023-06-10 18:16:08 +0800

    fix: savereminder Test

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -5,27 +5,30 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) :
     ReminderDataSource {
 
     //Create a fake data source to act as a double to the real data source
 
-
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        reminders?.let { return Result.Success(ArrayList(it)) }
-        return Result.Error("Reminders not found")
+        return try {
+            Result.Success(reminders)
+        } catch (ex: Exception) {
+            Result.Error(ex.localizedMessage)
+        }
     }
 
+
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminders?.add(reminder)
+        reminders.add(reminder)
     }
 
     override suspend fun deleteAllReminders() {
-        reminders?.clear()
+        reminders.clear()
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        val reminder = reminders?.find {
+        val reminder = reminders.find {
             it.id == id
         }
         return when {
