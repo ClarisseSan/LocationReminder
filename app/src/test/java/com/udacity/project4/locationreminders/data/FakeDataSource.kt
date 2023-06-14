ea83195a6aad422fa3b7379c0876d6d1d1996d90
Clarisse Sanchez <sanchezclarisse@gmail.com>
2023-06-14 12:12:24 +0800

    fix getReminder()

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,9 +1,7 @@
 package com.udacity.project4.locationreminders.data
 
-import com.google.android.gms.tasks.Task
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import java.lang.Error
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) :
@@ -12,14 +10,14 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) 
     //Create a fake data source to act as a double to the real data source
     private var shouldReturnError = false
 
-    fun setReturnError(value: Boolean){
+    fun setReturnError(value: Boolean) {
         shouldReturnError = value
     }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
 
-        if(shouldReturnError){
-            return  Result.Error("Reminders Error")
+        if (shouldReturnError) {
+            return Result.Error("Reminders Error")
         }
 
         return try {
@@ -39,13 +37,21 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) 
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        val reminder = reminders.find {
-            it.id == id
+
+        try {
+            val reminder = reminders.find {
+                it.id == id
+            }
+            if (reminder != null) {
+                return Result.Success(reminder)
+            } else {
+                return Result.Error("Reminder not found!")
+            }
+        } catch (e: Exception) {
+            return Result.Error(e.localizedMessage)
         }
-        return when {
-            reminder != null -> Result.Success(reminder)
-            else -> Result.Error("Reminder not found")
-        }
+
+
     }
 
 
